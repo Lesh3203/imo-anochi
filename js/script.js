27efb927edbff155a2afe7ae47e7c8ff79e9d56b
Lesh3203 <154484885+Lesh3203@users.noreply.github.com>
2026-01-21 18:20:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ $('.grid_item, button').css('border-radius','20px');
 
 $('.header').css('color','#266A82');
 
-$('.input').css({
+$('.input, button').css({
   'border-color':'#F9A268',
   'background-color':'#EBF1F3',
   'border-radius':'100px'
+  'padding':'12px'
+  'font-size':'16px'
+  'line-height':'24px'
 });
 
 $('.label').css({
