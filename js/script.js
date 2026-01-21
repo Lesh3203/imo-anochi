6e648a2e6574b8541f7d126c891b894abcd73325
Lesh3203 <154484885+Lesh3203@users.noreply.github.com>
2026-01-21 18:01:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
-$('.input').css('border-radius','100px')
+
 $('.grid_item').css('border-radius','20px')
 $('button').css('border-radius','20px')
+document.body.style.backgroundColor = "#ebf1f3";
+
+$('.header').css('color','#266A82')
+
+$('.input').css('border-color','#F9A268')
+$('.input').css('background-color','#EBF1F3')
+$('.input').css('border-radius','100px')
+
+$('.label').css('background-color','#EBF1F3')
+$('.label').css('color','#3D4548')
+
+.input {
+  background-color: #EBF1F3;
+  border: 1px solid #F9A268;
+  border-radius: 100px;
+}
+
+.green [type="checkbox"]:checked + label:before, .green [type="checkbox"]:not(:checked) + label:before {
+    border: 1px solid #F9A268;
+    border-radius: 10px;
+    background: #EBF1F3;
+}
+
+.label {
+  background-color: #EBF1F3;
+  color: #3D4548;
+}
+
+.header {
+  color: #266A82
+}
